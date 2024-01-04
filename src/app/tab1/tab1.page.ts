04204937c7b4d8559a572f359cd763c60d04562f
Vojtanooo <janecek.vojta@gmail.com>
2024-01-04 19:09:56 +0100

    Default pages setup

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  searchTerm: string = "";
+  foodInfo: any; // Objekt pro uchování informací o potravině
 
   constructor() {}
 
+  searchFood() {
+    // Simulace vyhledávání informací o potravině (můžeš nahradit vlastní logikou/API voláním)
+    this.foodInfo = {
+      name: this.searchTerm,
+      calories: 100,
+      proteins: 10,
+      carbs: 20,
+      fats: 5,
+    };
+  }
 }
